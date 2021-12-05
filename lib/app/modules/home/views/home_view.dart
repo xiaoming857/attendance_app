f0a922ae5f107c0eba427e55e5cc5eedfdbf2433
xiaoming857 <gnimoaix857@gmail.com>
2021-12-05 13:03:05 +0700

    Improve bottom navigation bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,37 +11,57 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () {
-        return Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: controller.currentIndex.value,
-            onTap: controller.onChangePage,
-            items: const [
-              BottomNavigationBarItem(
-                label: 'Dashboard',
-                icon: Icon(Icons.dashboard_outlined),
-              ),
-              BottomNavigationBarItem(
-                label: 'Profile',
-                icon: Icon(Icons.person_outline_rounded),
-              ),
-            ],
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+        child: SizedBox(
+          height: 50,
+          child: BottomAppBar(
+            clipBehavior: Clip.hardEdge,
+            shape: const CircularNotchedRectangle(),
+            notchMargin: 5.0,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                      child: const Icon(Icons.dashboard_outlined),
+                      onPressed: () => controller.onChangePage(0)),
+                ),
+                const SizedBox(width: 50),
+                Expanded(
+                  child: TextButton(
+                      child: const Icon(Icons.person_outline_rounded),
+                      onPressed: () => controller.onChangePage(1)),
+                )
+              ],
+            ),
           ),
-          body: Obx(
-            () {
-              switch (controller.currentIndex.value) {
-                case 0:
-                  return DashboardView();
-                case 1:
-                  return ProfileView();
-                default:
-                  return DashboardView();
-              }
-            },
-          ),
-        );
-      },
+        ),
+      ),
+      floatingActionButton: SizedBox.square(
+        dimension: 55,
+        child: FloatingActionButton(
+          child: const Icon(Icons.devices),
+          onPressed: () {},
+          elevation: 3,
+        ),
+      ),
+      body: Obx(
+        () {
+          switch (controller.currentIndex.value) {
+            case 0:
+              return DashboardView();
+            case 1:
+              return ProfileView();
+            default:
+              return DashboardView();
+          }
+        },
+      ),
     );
   }
 }
